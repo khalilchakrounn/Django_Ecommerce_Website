1b69bc19b691885ad2eb50679775ec4fc13b973c
khalilchakrounn <48619945+khalilchakrounn@users.noreply.github.com>
2022-11-08 20:27:25 +0100

    views and forms

--- testdajngo1/store/urls.py
+++ testdajngo1/store/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 app_name ='store'
 
+from store.views import product_creat_view, aboutView, contact
 
 urlpatterns = [
-    path('', views.all_products, name='all_products')
+    path('', views.all_products, name='all_products'),
+    path('about/', aboutView),
+    path('contact/', contact),
+    path('create/',product_creat_view),
 ]
--- testdajngo1/store/views.py
+++ testdajngo1/store/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 # Create your views here.
+from .forms import ProductForm
 
 def categories(request):
     return{
@@ -34,3 +35,15 @@ def aboutView(request, *args, **kwargs):
         "number": 1212
     }
     return render(request, 'store/about.html', my_context)
+
+
+def product_creat_view(request):
+    form= ProductForm(request.Post or None)
+    if form.is_valid():
+        form.save()
+        form= ProductForm()  # to clear the form and represent an empty one
+
+    context={
+        'form': form
+    }
+    return render ( request, "store/products/create.html", context)
