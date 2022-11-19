669c1805a14a0ee846341dbeeb514df97d19fb7d
khalilchakrounn <48619945+khalilchakrounn@users.noreply.github.com>
2022-11-20 00:22:44 +0100

    decorators

--- testdajngo1/courses/views.py
+++ testdajngo1/courses/views.py
@@ -111,3 +111,16 @@ class CourseView(CourseObjectMixin, View):
 def my_fbv(request, *args, **kwargs):
     print(request.method)
     return render(request, 'about.html', {})
+
+
+
+#decorators = [never_cache, login_required]
+
+#@method_decorator(decorators, name='dispatch')
+#class ProtectedView(TemplateView):
+    template_name = 'secret.html'
+
+#@method_decorator(never_cache, name='dispatch')
+#@method_decorator(login_required, name='dispatch')
+#class ProtectedView(TemplateView):
+    template_name = 'secret.html'
